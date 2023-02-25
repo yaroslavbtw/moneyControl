7728d2b016a458b4f1cd43b11f8610beaa0623db
yaroslav <yaroslavantonov47@gmail.com>
2023-02-25 18:33:35 +0300

    Cosmetic addons 3

--- expenses/static/js/summary.js
+++ expenses/static/js/summary.js
@@ -1,4 +1,5 @@
 const ctx = document.getElementById('myChart');
+const chr = document.getElementById('myChart2');
 
 const renderChart = (data_chart) =>{
         const name = data_chart.nameChart
@@ -35,10 +36,41 @@ const getChartData=()=>{
     $.ajax({
            type: "GET",
            url: url,
-           success: renderChart,
+           success: (data_chart)=>{
+               renderChart(data_chart);
+               renderChart2(data_chart);
+           } ,
            dataType: 'json',
        });
 };
 
-getChartData();
+window.onload =  getChartData;
 
+
+const renderChart2 = (data_chart)=> {
+    const name = data_chart.nameChart
+    const category_data = data_chart.category_data;
+    const [labels, data] = [Object.keys(category_data), Object.values(category_data)];
+    chr.style.height = '300px';
+    chr.style.width = '200px';
+    new Chart(chr, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: `Last 6 month ${name}`,
+                data: data,
+                borderWidth: 1,
+                backgroundColor: "#18BC9C",
+                borderColor: "#18BC9C",
+            }]
+        },
+        options: {
+            maintainAspectRatio: false,
+            title: {
+                display: true,
+                text: `${name} per category`,
+            },
+        }
+    });
+}
